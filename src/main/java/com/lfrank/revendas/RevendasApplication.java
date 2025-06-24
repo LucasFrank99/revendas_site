2aeda8500156984111e5e5237c338d7065cd5d5f
lucas frank <lucasfrank11@hotmail.com>
2025-06-24 19:28:19 -0300

    first commit

---
+++ src/main/java/com/lfrank/revendas/RevendasApplication.java
@@ -0,0 +1,13 @@
+package com.lfrank.revendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lfrank/revendas/RevendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lfrank.revendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
